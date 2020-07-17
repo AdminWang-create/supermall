1c2a207a3fa323147be9a81d3370608b6faff156
AdminWang <2367267971@qq.com>
2020-07-17 18:11:41 +0800

    '别名配置'

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-
-
+    <h2>abc</h2>
+    {{person}}
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
-
+  data(){
+    return{
+      person:{name:'kobe',age:25,height:1.8}
+    }
   }
 }
 </script>
 
 <style>
-
+@import "assets/css/base.css";
 </style>
